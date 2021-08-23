dc74f2859f6a9e3e26f381abf0eb2b5f22b3f4e7
Hadebe Mbali <hadebe@thedigitalacademy.co.za>
2021-08-23 16:21:07 +0200

    Backend

---
+++ Angular/students/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LandingComponent } from './components/landing/landing.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'landing', pathMatch: 'full'},
+  { path: 'landing', component: LandingComponent},
+  { path: 'home', component: HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
